d477ef9038c2989440700671e2fe031385bb15f8
Jose Luis Koller <joseluiskoller98@gmail.com>
2024-06-22 15:07:52 -0300

    2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/events",
+        destination: "https://tech-events-argy.vercel.app/api/events",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
