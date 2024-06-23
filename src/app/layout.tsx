e8ab5c483ee1cdcf30e11470b2ca74b574aa64c4
Jose Luis Koller <joseluiskoller98@gmail.com>
2024-06-22 21:02:15 -0300

    a ver esto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import { Libre_Franklin } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
 import { Providers } from "./providers";
 import { Metadata } from "next";
 import "./styles/globals.css";
@@ -9,6 +11,17 @@ export const metadata: Metadata = {
   description: "Una recopilación de eventos tech en Argentina.",
 };
 
+const libre_franklin = Libre_Franklin({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-libre_franklin",
+});
+const cormorant_garamond = Cormorant_Garamond({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-cormorant_garamond",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -16,7 +29,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang='es' suppressHydrationWarning>
-      <body className='flex flex-col place-items-center'>
+      <body
+        className={
+          libre_franklin.variable +
+          " " +
+          cormorant_garamond.variable +
+          " flex flex-col place-items-center"
+        }
+      >
         <Providers>
           <Header />
           {children}
